952d9e8b17e45876baa15605e97e317d2ba3958f
Joshua Allard <22JAllard@tgschool.net>
2024-03-15 12:45:31 +0000

    stuff

--- import pygame, sys.py
+++ import pygame, sys.py
@@ -6,21 +6,29 @@ green = (0, 255, 0)
 width = 1360
 height = 710
 
-bird = pygame.Rect(height // 2, 100, 50, 50)
+gravity = 0.7
+bird_height = 350
+birdy = pygame.image.load('bird.png')
+bird2 = pygame.transform.scale(birdy, (50,50))
 
 screen = pygame.display.set_mode((width, height))
 pygame.display.set_caption('Floppy bird')
 
 run = True
 while run:
-    for event in pygame.event.get():
-        screen.fill(green)
-        pygame.draw.rect(screen, white, bird)
+    screen.fill(green)
+    screen.blit(bird2, (100, bird_height))
+
+    key = pygame.key.get_pressed()
+
+    if key[pygame.K_SPACE]:
+        bird_height -= 4
+    bird_height += gravity
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     pygame.display.update()
-
 pygame.quit()
+sys.exit()
