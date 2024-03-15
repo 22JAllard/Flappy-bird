88b9e022ee8cba2cbe0b7eb040be866347f1ca98
Joshua Allard <22JAllard@tgschool.net>
2024-03-15 12:54:15 +0000

    moving bird

--- import pygame, sys.py
+++ import pygame, sys.py
@@ -1,11 +1,15 @@
 import pygame, sys
 from pygame.locals import *
+import time
 
 pygame.init()
 green = (0, 255, 0)
+white = (0, 0, 0)
 width = 1360
 height = 710
 
+font = pygame.font.SysFont('Ariel', 60)
+score = 0
 gravity = 0.7
 bird_height = 350
 birdy = pygame.image.load('bird.png')
@@ -22,12 +26,15 @@ while run:
     key = pygame.key.get_pressed()
 
     if key[pygame.K_SPACE]:
-        bird_height -= 4
+        bird_height -= 6
     bird_height += gravity
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    score_text = font.render("Score: " + str(score), True, white)
+    screen.blit(score_text, (10, 10))
 
     pygame.display.update()
 pygame.quit()
