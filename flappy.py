83e1c96baa39bc8bf25956031cc91205eabda2cf
Joshua Allard <22JAllard@tgschool.net>
2024-05-14 10:09:48 +0100

    sky score stuff

--- flappy.py
+++ flappy.py
@@ -4,10 +4,10 @@ import time
 import random
 
 pygame.init()
-green = (0, 255, 0)
+green = (69, 200, 69)
 white = (255, 255, 255)
 width = 1360
-height = 710
+height = 610
 
 font = pygame.font.SysFont('Ariel', 60)
 score = 0
@@ -16,6 +16,8 @@ bird_height = 350
 birdy = pygame.image.load('bird.png')
 bird2 = pygame.transform.scale(birdy, (50,50))
 
+sky = pygame.image.load('sky.jpg')
+skyimg = pygame.transform.scale(sky, (width, height))
 random1 = random.randint(100,(height-100))
 speed = 30
 
@@ -31,18 +33,27 @@ def towers():
 
 run = True
 while run:
-    screen.fill(green)
+    screen.blit(skyimg, (0,0))
     screen.blit(bird2, (100, bird_height))
 
     key = pygame.key.get_pressed()
 
     if key[pygame.K_SPACE]:
-        bird_height -= 4
-    bird_height += gravity
+        if bird_height < 0:
+            bird_height = 0
+        else:
+            bird_height -= 4
+    if bird_height + bird2.get_height() >= height:
+        bird_height = height - bird2.get_height()
+    else:        
+        bird_height += gravity
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
+
+    if pygame.time.get_ticks() % 500 == 0:
+        score += 1
 
     score_text = font.render("Score: " + str(score), True, white)
     screen.blit(score_text, (10, 10))
